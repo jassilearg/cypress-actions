4fda4e933fdbd855a53dc09dee65dc16454b1a24
Jassiléa Góis <86447182+jassilearg@users.noreply.github.com>
2025-04-09 16:11:56 -0300

    Integrando testes com Tesults.js

---
+++ runner.js
@@ -0,0 +1,15 @@
+const cypress = require('cypress')
+const tesults = require('./cypress-tesults-reporter');
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjkxNTg1YmM3LTcwNzItNDM3OS1hYjYwLThiYTc3NjI0Mjc5NC0xNzQ0MjI1Mzk4ODA5IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiYzdhYWYwMjctZWM0ZC00OGM2LWJhZWItMjg0ZmRlMjg0NWUzIiwidHlwZSI6InQifQ.s4VAusIohXKJK7_TWxUlPSMmK9UNyYJZ47IDLQHso7w
+cypress.run({
+  
+})
+.then((results) => {
+  const args = {
+      target: TOKEN,
+  }
+  tesults.results(results, args);
+})
+.catch((err) => {
+  console.error(err)
+})
